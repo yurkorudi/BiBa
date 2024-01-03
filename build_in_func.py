e6778ead5c066c977b40690c14a7f5c00bf38592
yurkorudi <48592176+yurkorudi@users.noreply.github.com>
2024-01-03 17:13:34 +0200

    to main files
    ....

--- build_in_func.py
+++ build_in_func.py
@@ -2,3 +2,38 @@
 class Welcome_func:
     def __init__(self):
         print('hello world')
+
+
+
+
+class Main_functions: 
+    def __init__(self, client):
+        self.client = client
+        self.speech_output_path = './speech_outputs'
+
+
+    def speech_to_text(self, file):
+        self.file = open(file, 'rb')
+        self.transcript = self.client.audio.translations.create(
+            model="whisper-1", 
+            file= self.file,
+            response_format="text"
+        )
+        return self.transcript
+    
+
+    def text_to_speech(self, text):
+        self.speech = self.client.speech.create(
+            model='tts-1',
+            voice='fable',
+            input=text
+        )
+        self.speech.stream_to_file(self.speech_output_path)
+        
+
+
+
+
+
+
+    
--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from app_conectivity import Connect
 from resemble import Resemble
 from build_in_func import Welcome_func
-
+from openai import OpenAI
+from pathlib import Path
+from build_in_func import Main_functions
 
 app = Connect()
 welcome = Welcome_func()
 
-Resemble.api_key('')
+client = OpenAI(api_key=open("./keys/biba_key.txt", 'r').read())
+
+
 
