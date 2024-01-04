879d4cafe88baffccdd8a4091e5b25ced2574500
PaulStefanyshyn <126797697+PaulStefanyshyn@users.noreply.github.com>
2024-01-04 14:07:37 +0200

    android_update

---
+++ _20240104_BibaMobileApp/_20240104_BibaMobileApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using _20240104_BibaMobileApp.Services;
+using _20240104_BibaMobileApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace _20240104_BibaMobileApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<LocalDataStoreSlot>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
